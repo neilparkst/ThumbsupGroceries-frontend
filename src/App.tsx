debe9f9c23cae7fa7965d98132a063d8f3a240ab
Neil Park <neil.park.st@gmail.com>
2025-04-28 12:50:15 +1200

    setup react query on app

--- src/App.tsx
+++ src/App.tsx
@@ -22,41 +22,52 @@ import { getStore } from './Data/GlobalState/Store';
 import { Provider } from 'react-redux';
 import NotFoundPage from './Pages/NotFoundPage';
 import Header from './Components/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const store = getStore();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false
+    }
+  }
+});
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path='' element={<HomePage />} />
-            <Route path='signin' element={<SignInPage />} />
-            <Route path='signup' element={<SignUpPage />} />
-            <Route path='membershipsubscription' element={<MembershipSubscriptionPage />} />
-            <Route path='account' element={<Account />}>
-              <Route path='details' element={<PersonalDetailsPage />} />
-              <Route path='orders' element={<MyOrdersPage />} />
-              <Route path='membership' element={<MyMembershipStatusPage />} />
-            </Route>
-            <Route path='trolley' element={<TrolleyPage />} />
-            <Route path='products' element={<Products />}>
-              <Route index element={<ProductListPage />} />
-              <Route path='categories/:categoryId' element={<ProductListPage />} />
-              <Route path=':productId' element={<ProductDetailPage />} />
-            </Route>
-            <Route path='admin' element={<Admin />}>
-              <Route path='products' element={<AdminProducts />}>
-                <Route index element={<AdminProductListPage />} />
-                <Route path='categories/:categoryId' element={<AdminProductListPage />} />
-                <Route path=':productId' element={<AdminProductDetailPage />} />
-                <Route path='new' element={<AdminProductAddPage />} />
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path='' element={<HomePage />} />
+              <Route path='signin' element={<SignInPage />} />
+              <Route path='signup' element={<SignUpPage />} />
+              <Route path='membershipsubscription' element={<MembershipSubscriptionPage />} />
+              <Route path='account' element={<Account />}>
+                <Route path='details' element={<PersonalDetailsPage />} />
+                <Route path='orders' element={<MyOrdersPage />} />
+                <Route path='membership' element={<MyMembershipStatusPage />} />
               </Route>
-            </Route>
-            <Route path='*' element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
+              <Route path='trolley' element={<TrolleyPage />} />
+              <Route path='products' element={<Products />}>
+                <Route index element={<ProductListPage />} />
+                <Route path='categories/:categoryId' element={<ProductListPage />} />
+                <Route path=':productId' element={<ProductDetailPage />} />
+              </Route>
+              <Route path='admin' element={<Admin />}>
+                <Route path='products' element={<AdminProducts />}>
+                  <Route index element={<AdminProductListPage />} />
+                  <Route path='categories/:categoryId' element={<AdminProductListPage />} />
+                  <Route path=':productId' element={<AdminProductDetailPage />} />
+                  <Route path='new' element={<AdminProductAddPage />} />
+                </Route>
+              </Route>
+              <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
       </Provider>
     </div>
   );
