e0c7df69427bd014a85786d13b446416558d1c9f
Neil Park <neil.park.st@gmail.com>
2025-04-29 18:20:54 +1200

    implement ProductListPage elements

--- src/Pages/Products/ProductListPage.tsx
+++ src/Pages/Products/ProductListPage.tsx
@@ -1,15 +1,25 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { ChangeEvent, useEffect, useMemo, useRef, useState } from 'react';
 import './ProductListPage.scss';
 import { useQuery } from '@tanstack/react-query';
-import { useNavigate, useParams } from 'react-router-dom';
-import { CategoryTree, getCategoryTree } from '../../Data/ProductData';
-import { Box, Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { CategoryTree, getCategoryTree, getProducts, ProductSimple } from '../../Data/ProductData';
+import { Box, Divider, FormControl, IconButton, InputAdornment, List, ListItem, ListItemIcon, ListItemText, MenuItem, Pagination, Select, SelectChangeEvent, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { getCategoryNameById, getTreePath } from '../../Data/Util';
+import ProductCard from '../../Components/ProductCard';
 
 const ProductListPage = () => {
     const params = useParams();
-    const categoryId = parseInt(params.categoryId || '0');
+    const categoryId = params.categoryId ? parseInt(params.categoryId) : undefined;
+
+    const [ searchParams ] = useSearchParams();
+    const search = searchParams.get('search');
+    const sort = searchParams.get('sort');
+    const page = parseInt(searchParams.get('page') || '1');
+    const pageSize = parseInt(searchParams.get('pageSize') || '24');
+
+    const navigate = useNavigate();
 
     const { data: categoryTree } = useQuery({
         queryKey: ['categoryTree'],
@@ -27,7 +37,7 @@ const ProductListPage = () => {
         if((categoryTree?.length || 0 ) === categoryTreeLengthRef.current){
             return;
         }
-        return getCategoryNameById(categoryTree, categoryId);
+        return getCategoryNameById(categoryTree, categoryId || 0);
     }, [categoryId, categoryTree])
 
     return (
@@ -36,7 +46,7 @@ const ProductListPage = () => {
                 <CategoryList
                     key={categoryId}
                     categoryTree={categoryTree}
-                    initialCategoryId={categoryId}
+                    initialCategoryId={categoryId || 0}
                 />
             </div>
             <div className="ProductListContainer">
@@ -44,16 +54,74 @@ const ProductListPage = () => {
                     {categoryName}
                 </div>
                 <div className="SearchAndSort">
+                    <Box
+                        className='SearchBar'
+                        component='form'
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            const data = new FormData(e.currentTarget);
+                            const search = data.get('search');
 
-                </div>
-                <div className="ProductList">
-                    <div className="ProductCardContainer">
+                            navigate(`/products/categories/${categoryId}?search=${search}`);
+                        }}
+                        style={{
+                            visibility: !categoryId ? 'hidden' : 'initial'
+                        }}
+                    >
+                        <TextField
+                            name='search'
+                            placeholder='In this category...'
+                            variant='standard'
+                            fullWidth
+                            slotProps={{
+                                input: {
+                                    disableUnderline: true,
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton type="submit" edge="end">
+                                                <SearchIcon sx={{ color: '#1976d2' }} />
+                                            </IconButton>
+                                        </InputAdornment>
+                                    )
+                                }
+                            }}
+                        />
+                    </Box>
+                    <div className="SortContainer">
+                        <span>Sort By</span>
+                        <FormControl fullWidth>
+                            <Select
+                                id="sortMethod-selection"
+                                value={sort || 'relevance'}
+                                label="Sort by"
+                                onChange={(event: SelectChangeEvent) => {
+                                    const newSort = event.target.value;
+                                    let currentUrl = new URL(window.location.href);
 
+                                    [{search}, {sort: newSort}, {page}, {pageSize}].forEach(item => {
+                                        const [[key, value]] = Object.entries(item);
+                                        if(value){
+                                            currentUrl.searchParams.set(key, value);
+                                        }
+                                    })
+
+                                    navigate(currentUrl.pathname + currentUrl.search);
+                                }}
+                            >
+                                {[{name: 'Relevance', value: 'relevance'}, {name: 'Low Price', value: 'priceLow'}, {name: 'High Price', value: 'priceHigh'}].map(sortMethod => (
+                                    <MenuItem key={sortMethod.value} value={sortMethod.value} >{sortMethod.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </div>
-                    <div className="PageNavigation">
-
-                    </div>
                 </div>
+                <ProductList
+                    categoryId={categoryId}
+                    search={search || undefined}
+                    sort={sort || undefined}
+                    page={page}
+                    pageSize={pageSize}
+                />
             </div>
         </div>
     );
@@ -153,6 +221,60 @@ const CategoryList = ({categoryTree, initialCategoryId} : {categoryTree: Categor
                 )}
             </List>
         </Box>
+    )
+}
+
+const ProductList = ({categoryId, search, sort, page, pageSize} : {categoryId?: number, search?: string, sort?: string, page?: number, pageSize?: number}) => {
+    const navigate = useNavigate();
+    
+    const {data: products} = useQuery({
+        queryKey: ['products', {categoryId: categoryId, sort: sort, search: search, page: page, pageSize: pageSize}],
+        queryFn: async () => {
+            const response = await getProducts(categoryId, sort, search, page, pageSize);
+            if('errorMessage' in response){
+                throw new Error(response.errorMessage);
+            }
+
+            return response as ProductSimple[];
+        }
+    })
+
+    if(products && products.length === 0){
+        return(
+            <div className='NoProduct'>
+                No Products
+            </div>
+        )
+    }
+    
+    return(
+        <div className="ProductList">
+            <div className="ProductCardContainer">
+                {products?.map(product => (
+                    <ProductCard key={product.productId} product={product} />
+                ))}
+            </div>
+            <div className="Pagination">
+                <Pagination
+                    count={Math.ceil((products?.length || 0) / (pageSize || 24))}
+                    page={page}
+                    onChange={(event: ChangeEvent<unknown>, value) => {
+                        const newPage = value;
+
+                        let currentUrl = new URL(window.location.href);
+
+                        [{search}, {sort}, {page: newPage}, {pageSize}].forEach(item => {
+                            const [[key, value]] = Object.entries(item);
+                            if(value){
+                                currentUrl.searchParams.set(key, value);
+                            }
+                        })
+
+                        navigate(currentUrl.pathname + currentUrl.search);
+                    }}
+                />
+            </div>
+        </div>
     )
 }
 
