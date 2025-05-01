49dfeaec3222c73fdc459302f2c946bdbc2b7999
Neil Park <neil.park.st@gmail.com>
2025-05-02 00:26:53 +1200

    delete initial react create app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/Home/HomePage';
 import SignInPage from './Pages/SignIn/SignInPage';
