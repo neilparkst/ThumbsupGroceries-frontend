9e055eaa50086f3238a6a88de727591819c79586
Neil Park <neil.park.st@gmail.com>
2025-05-22 23:01:40 +1200

    setup for deployment

--- src/Data/Settings.ts
+++ src/Data/Settings.ts
@@ -1,4 +1,4 @@
-export const domain = "https://localhost:7192";
+export const domain = process.env.REACT_APP_ENV === 'production' ? "https://thumbsupgroceries-backend20250522175724.azurewebsites.net/" : "https://localhost:7192";
 
 export const webAPIUrl = `${domain}/api/v1`;
 
